136a72345560ee650fc87c7ac9b146e1f49fb007
MagnumOpus <arifraihan062003@gmail.com>
2025-05-27 20:52:33 +0800

    Enhancement

--- Computer-Science/Virtualization/Docker/MySQL/init.bat
+++ Computer-Science/Virtualization/Docker/MySQL/init.bat
@@ -1,6 +1,15 @@
 @echo off
 
 echo MYSQL DATABASE INITIATOR 
+
+
+echo MYSQL VERSION : 
+
+
+
+powershell -Command "Invoke-WebRequest 'https://registry.hub.docker.com/v2/repositories/library/mysql/tags?page_size=100' | Select-Object -ExpandProperty Content | ConvertFrom-Json | ForEach-Object { $_.results } | ForEach-Object { $_.name }"
+
+
 SET /P version=">> Version : "
 SET /P port=">> Port : "
 SET /P database=">> Database : "
@@ -50,7 +59,6 @@ IF NOT EXIST "%config_file_path%" (
 	echo Creating default MySQL config file : %config_file_path%
 	(
 		echo [mysqld]
-		echo skip-host-cache
 		echo skip-name-resolve
 		echo sql_mode=STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION
 	) > "%config_file_path%"
@@ -59,8 +67,17 @@ IF NOT EXIST "%config_file_path%" (
 )
 
 
+REM Changing %config_file_path% permission
+icacls "%config_file_path%" /inheritance:r
+icacls "%config_file_path%" /grant:r "Administrators:(R,W)"
+icacls "%config_file_path%" /grant:r "SYSTEM:(R,W)"
+icacls "%config_file_path%" /grant:r "Users:(R)"
+icacls "%config_file_path%" /grant:r "Administrators:(F)"
+icacls "%config_file_path%" /grant:r "SYSTEM:(F)"
+
+
 REM Docker Run Command
-docker run -d --name %container_name% -p %port%:3306 -e MYSQL_ROOT_PASSWORD=%root_password% -e MYSQL_DATABASE=%database% -v %data_dir_path%:/var/lib/mysql -v %config_file_path%:/etc/mysql/my.cnf mysql:%version%
+docker run -d --name %container_name% -p %port%:3306 -e MYSQL_ROOT_PASSWORD=%root_password% -e MYSQL_DATABASE=%database% -v %data_dir_path%:/var/lib/mysql -v %config_file_path%:/etc/mysql/my.cnf:ro mysql:%version%
 
 
 
